abe5115f541634e8fbb20ac18bca57c7d9697ec7
Mike <wattonlaw@gmail.com>
2021-11-22 00:03:10 -0600

    commit

--- object_enhancements_exercise.js
+++ object_enhancements_exercise.js
@@ -22,6 +22,15 @@ const instructor = {
     }
 }
 
+
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
+
 function createAnimal(species, verb, noise) {
     return {
         species,
